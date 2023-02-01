d94922d498021fc0028210edd8f50e6b1d51f761
Castillo <gpro1pro@gmail.com>
2023-01-31 21:40:06 -0500

    history transfer added

--- controllers/transfers.controller.ts
+++ controllers/transfers.controller.ts
@@ -3,49 +3,57 @@ import TransferModel from '../models/transfer.models';
 import UserModel from '../models/user.models';
 
 const transferAmount = async (req: Request, res: Response) => {
-  const { amount, senderUserId, reciverUserId } = req.body;
+  try {
+    const { amount, senderUserId, reciverUserId } = req.body;
 
-  if (senderUserId == reciverUserId)
-    return res.status(404).json({
-      status: 'error',
-      message: 'you can not transfer yourself',
+    if (senderUserId == reciverUserId)
+      return res.status(404).json({
+        status: 'error',
+        message: 'you can not transfer yourself',
+      });
+    const senderUser = await UserModel.findOne({
+      where: {
+        accountNumber: senderUserId,
+      },
     });
-  const senderUser = await UserModel.findOne({
-    where: {
-      accountNumber: senderUserId,
-    },
-  });
-  const reciverUser = await UserModel.findOne({
-    where: {
-      accountNumber: reciverUserId,
-    },
-  });
-  if (!senderUser || !reciverUser)
-    return res.status(404).json({
-      status: 'error',
-      message: 'this account not exist',
+    const reciverUser = await UserModel.findOne({
+      where: {
+        accountNumber: reciverUserId,
+      },
     });
-  if (senderUser.amount < amount)
-    return res.status(404).json({
-      status: 'error',
-      message: 'amount exceeded',
-    });
+    if (!senderUser || !reciverUser)
+      return res.status(404).json({
+        status: 'error',
+        message: 'this account not exist',
+      });
+    if (senderUser.amount < amount)
+      return res.status(404).json({
+        status: 'error',
+        message: 'amount exceeded',
+      });
 
-  const senderUserAmount = +senderUser.amount - amount;
-  await senderUser.update({ amount: senderUserAmount });
-  const reciverUserAmount = +reciverUser.amount + amount;
-  await reciverUser.update({ amount: reciverUserAmount });
+    const senderUserAmount = +senderUser.amount - amount;
+    await senderUser.update({ amount: senderUserAmount });
+    const reciverUserAmount = +reciverUser.amount + amount;
+    await reciverUser.update({ amount: reciverUserAmount });
 
-  const transfer = await TransferModel.create({
-    amount,
-    senderUserId,
-    reciverUserId,
-  });
-  res.json({
-    status: 'succes',
-    message: 'transfer completed',
-    transfer,
-  });
+    const transfer = await TransferModel.create({
+      amount,
+      senderUserId,
+      reciverUserId,
+    });
+    res.json({
+      status: 'succes',
+      message: 'transfer completed',
+      transfer,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  }
 };
 
 export { transferAmount };
--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,47 +1,98 @@
 import { Request, Response } from 'express';
+import { where } from 'sequelize';
+import TransferModel from '../models/transfer.models';
 import UserModel from '../models/user.models';
 import getAccountNumber from '../utils/accountNumber.handle';
 
-const register = async (req: Request, res: Response)=> {
-  const { name, password } = req.body;
-  const accountNumber = await getAccountNumber();
-  const createUser = await UserModel.create({
-    name,
-    password,
-    accountNumber,
-  });
-  res.send({
-    status: 'success',
-    message: 'user created successfully',
-    createUser,
-  });
+const register = async (req: Request, res: Response) => {
+  try {
+    const { name, password } = req.body;
+    const accountNumber = await getAccountNumber();
+    const createUser = await UserModel.create({
+      name,
+      password,
+      accountNumber,
+    });
+    res.send({
+      status: 'success',
+      message: 'user created successfully',
+      createUser,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  }
 };
 const login = async (req: Request, res: Response) => {
-  const { accountNumber, password } = req.body;
-  const user = await UserModel.findOne({
-    where: {
-      accountNumber,
-    },
-  });
-  if (!user)
-    return res.status(404).json({
-      status: 'error',
-      message: 'you need an account to Log in',
+  try {
+    const { accountNumber, password } = req.body;
+    const user = await UserModel.findOne({
+      where: {
+        accountNumber,
+      },
     });
-  if (user?.password != password)
-    return res.status(404).json({
-      status: 'error',
-      message: 'Incorrect password',
+    if (!user)
+      return res.status(404).json({
+        status: 'error',
+        message: 'you need an account to Log in',
+      });
+    if (user?.password != password)
+      return res.status(404).json({
+        status: 'error',
+        message: 'Incorrect password',
+      });
+    res.json({
+      status: 'success',
+      message: 'user logged successfully',
+      user,
     });
-  res.send({
-    status: 'success',
-    message: 'user logged successfully',
-    user,
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  }
 };
 
-const historyTransfer = (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.json({id})
+const historyTransfer = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = await UserModel.findOne({
+      where: {
+        id,
+        status: 'available',
+      },
+    });
+    if (!user)
+      return res.status(404).json({
+        status: 'error',
+        message: 'User not found',
+      });
+    const transfer = await TransferModel.findAll({
+      where: {
+        senderUserId: +user.accountNumber,
+      },
+    });
+    if (transfer.length === 0)
+      return res.status(404).json({
+        status: 'error',
+        message: 'The user has no transfers',
+      });
+    res.json({
+      status: 'success',
+      message: 'get transfer succesfully',
+      transfer,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  }
 };
 export { register, login, historyTransfer };
